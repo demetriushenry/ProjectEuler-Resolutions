9cfa4cb6b666035915d9a26acbe410efc7c042c8
Demetrius <demetriushenry@users.noreply.github.com>
2017-08-27 17:30:59 -0400

    Project Euler #4 Solution
    solution for project euler #4

---
+++ ProjectEuler04.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProjectEuler04 {
+
+	static boolean isPalindrome(int n) {
+		int tmp = n;
+		int sum = 0;
+		int r;
+		
+		while (tmp != 0) {
+			r = tmp % 10;
+			tmp = tmp / 10;
+			sum = sum * 10 + r;
+		}
+		
+		return n == sum;
+	}
+	
+	static int getLargestPalindrome(int n) {
+		List<Integer> compareList = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+			if (isPalindrome(i))
+				compareList.add(i);
+		}
+		
+		System.out.println(compareList.toString());
+		
+		int max = Collections.max(compareList);
+		
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int t = Math.abs(in.nextInt());
+        int[] list = new int[t];
+        for(int i = 0; i < t; i++){
+            int n = Math.abs(in.nextInt());
+            list[i] = getLargestPalindrome(n);
+        }
+        
+        for (int l : list) {
+			System.out.println("" + l);
+		}
+        
+        in.close();
+	}
+
+}
