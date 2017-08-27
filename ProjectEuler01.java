9924c8a9ead62de546c25147ee5af7d6b7a3a2e7
Demetrius <demetriushenry@users.noreply.github.com>
2017-08-27 12:25:20 -0400

    ProjectEuler #1
    Resolution to problem #1 from Project Euler

---
+++ ProjectEuler01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Hello {
+	
+	static int filterMultiple(int count) {
+		int result = 0;
+		for (int i = 0; i < count; i++) {
+			if (/*(i >= 3) &&*/ (i % 3 == 0 || i % 5 == 0)) {
+				result += i;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int[] count = new int[t];
+		
+		for (int i = 0; i < t; i++) {
+			int n = Math.abs(in.nextInt());
+			count[i] = filterMultiple(n);
+		}
+		
+		for (int i = 0; i < count.length; i++) {
+			System.out.println("" + count[i]);
+		}
+		
+	}
+
+}
